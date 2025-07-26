30f3b32f48637d71e41b70d5bbe6f3445f412ce3
ProgrammerNafis <sahariyanafis974@gmail.com>
2025-07-26 13:22:17 +0600

    First From Spck

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,7 @@
 """
-URL configuration for myproject project.
-
+done first 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+    https://docs.djangoproj     ect.com/en/5.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
